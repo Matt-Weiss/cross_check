9b26e4e87c967b4336633d19d36e1d5aa19635f6
David Hardy <davehardy632@gmail.com>
2019-02-14 12:26:02 -0700

    Started to setup teams class

---
+++ lib/teams.rb
@@ -0,0 +1,19 @@
+require 'csv'
+
+class Teams
+  attr_reader :team_id,
+              :franchise_id,
+              :short_name,
+              :team_name,
+              :abbreviation,
+              :link
+  def initialize(row)
+    @team_id = row["team_id"]
+    @franchise_id = row["franchiseId"]
+    @short_name = row["shortName"]
+    @team_name = row["teamName"]
+    @abbreviation = row["abbreviation"]
+    @link = row["Link"]
+  end
+
+end
---
+++ test/teams_test.rb
@@ -0,0 +1,11 @@
+# require './test/test_helper'
+#
+# class TeamsTest < Minitest::Test
+#   def setup
+#     team_path = './data/team_info.csv'
+#     "team_id","franchiseId","shortName","teamName","abbreviation","link"
+#   end
+#
+#
+#
+# end
