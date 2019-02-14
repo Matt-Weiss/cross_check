97cf5ed9b9c9eeade92fbba5ea4989e928e08d7f
David Hardy <davehardy632@gmail.com>
2019-02-14 15:26:36 -0700

    Started iteration three organizing methods into new module.

---
+++ test/league_and_season_stats_test.rb
@@ -0,0 +1,26 @@
+require './test/test_helper'
+
+
+class LeagueAndSeasonStatsTest < Minitest::Test
+
+  def setup
+    game_path = './data/game_test.csv'
+    team_path = './data/team_info.csv'
+    game_teams_path = './data/game_teams_stats_test.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+  def test_count_of_teams
+    assert_equal 33, @stat_tracker.count_of_teams
+  end
+
+
+
+end
