8ba4b7cbb0e8976c17b0d61511e552f65ca1bc45
Matt Weiss <weiss.matt@outlook.com>
2019-02-15 17:52:52 -0700

    Add more Iteration 3 methods and tests

--- test/league_and_season_stats_test.rb
+++ test/league_and_season_stats_test.rb
@@ -28,22 +28,40 @@ class LeagueAndSeasonStatsTest < Minitest::Test
     @stat_tracker.goals_allowed
   end
 
-  def test_best_defense
-    assert_equal "Boston Bruins",  @stat_tracker.best_defense
+  def test_best_offense
+    assert_equal "Boston Bruins", @stat_tracker.best_offense
   end
 
-  def test_avg_goals_scored_at_home #helper
-    @stat_tracker.goals_scored_at_home
+  def test_worst_offense
+    assert_equal "Pittsburgh Penguins", @stat_tracker.worst_offense
+  end
+
+  def test_best_defense
+    assert_equal "Boston Bruins", @stat_tracker.best_defense
+  end
+
+  def test_worst_defense
+    assert_equal "Anaheim Ducks", @stat_tracker.worst_defense
+  end
+
+  def test_highest_scoring_visitor
+    assert_equal "Ottawa Senators", @stat_tracker.highest_scoring_visitor
   end
 
   def test_highest_scoring_home_team
     assert_equal "Ottawa Senators", @stat_tracker.highest_scoring_home_team
   end
 
-
-
-  def test_goals_allowed
-    binding.pry
-    assert_equal 12, @stat_tracker.best_offense
+  def test_lowest_scoring_visitor
+    assert_equal "Pittsburgh Penguins", @stat_tracker.lowest_scoring_visitor
   end
+
+  def test_lowest_scoring_home_team
+    assert_equal "Pittsburgh Penguins", @stat_tracker.lowest_scoring_home_team
+  end
+
+  def test_avg_goals_scored_at_home #helper
+    @stat_tracker.goals_scored_at_home
+  end
+
 end
