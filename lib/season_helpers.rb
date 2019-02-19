1e668d521e494156024221821fb2ffa3a7e7b77c
Matt Weiss <weiss.matt@outlook.com>
2019-02-18 22:31:05 -0700

    refactor game_ids by season for speed

--- lib/season_helpers.rb
+++ lib/season_helpers.rb
@@ -1,28 +1,26 @@
 module SeasonStatsHelpers
   def preseason_games(season)
-    preseason_games = []
+    @preseason_games = []
     games.each do |game|
       if game.season == season && game.type == "P"
-        preseason_games << game.game_id
+        @preseason_games << game.game_id
       end
     end
-    preseason_games
   end
 
   def regular_season_games(season)
-    regular_season_games = []
+    @regular_season_games = []
     games.each do |game|
       if game.season == season && game.type == "R"
-        regular_season_games << game.game_id
+        @regular_season_games << game.game_id
       end
     end
-    regular_season_games
   end
 
-  def preseason_win_percentage(season)
+  def preseason_win_percentage
     preseason_wins_by_team = Hash.new {|hash, key| hash[key] = [0,0]}
     game_teams.each do |game|
-      if preseason_games(season).include?(game.game_id)
+      if @preseason_games.include?(game.game_id)
         preseason_wins_by_team[game.team_id][1] += 1
         if game.won == "TRUE"
           preseason_wins_by_team[game.team_id][0] += 1
@@ -36,10 +34,10 @@ module SeasonStatsHelpers
     preseason_win_percentage
   end
 
-  def regular_season_win_percentage(season)
+  def regular_season_win_percentage
     regular_season_wins_by_team = Hash.new {|hash, key| hash[key] = [0,0]}
     game_teams.each do |game|
-      if regular_season_games(season).include?(game.game_id)
+      if @regular_season_games.include?(game.game_id)
         regular_season_wins_by_team[game.team_id][1] += 1
         if game.won == "TRUE"
           regular_season_wins_by_team[game.team_id][0] += 1
@@ -54,7 +52,9 @@ module SeasonStatsHelpers
   end
 
   def preseason_vs_regular_season(season)
-    win_differential = preseason_win_percentage(season).merge(regular_season_win_percentage(season)){
+    preseason_games(season)
+    regular_season_games(season)
+    win_differential = preseason_win_percentage.merge(regular_season_win_percentage){
       |key, preseason, regular_season| (regular_season - preseason).round(2)
     }
     sorted_win_diff = win_differential.sort_by do |key, value|
@@ -64,19 +64,15 @@ module SeasonStatsHelpers
   end
 
   def all_games_in_season(season)
-    full_season_games = []
-    games.each do |game|
-      if game.season == season
-        full_season_games << game.game_id
-      end
-    end
-    full_season_games
+    preseason_games(season)
+    regular_season_games(season)
+    @full_season_games = @preseason_games + @regular_season_games
   end
 
-  def coach_wins(season)
+  def coach_wins
     full_season_wins_by_coach = Hash.new {|hash, key| hash[key] = [0,0]}
     game_teams.each do |game|
-      if all_games_in_season(season).include?(game.game_id)
+      if @full_season_games.include?(game.game_id)
         full_season_wins_by_coach[game.head_coach][1] += 1
         if game.won == "TRUE"
           full_season_wins_by_coach[game.head_coach][0] += 1
@@ -87,8 +83,9 @@ module SeasonStatsHelpers
   end
 
   def coach_win_percentage(season)
+    all_games_in_season(season)
     full_season_win_percentage = {}
-    (coach_wins(season)).each do |key, value|
+    (coach_wins).each do |key, value|
       full_season_win_percentage[key] = (value[0].to_f / value[1]).round(4)
     end
     full_season_sorted = full_season_win_percentage.sort_by do |key, value|
@@ -97,10 +94,10 @@ module SeasonStatsHelpers
     full_season_sorted
   end
 
-  def accuracy_data(season)
+  def accuracy_data
     accuracy_by_team = Hash.new {|hash, key| hash[key] = [0,0]}
     game_teams.each do |game|
-      if all_games_in_season(season).include?(game.game_id)
+      if @full_season_games.include?(game.game_id)
         accuracy_by_team[game.team_id][1] += game.shots
         accuracy_by_team[game.team_id][0] += game.goals
       end
@@ -109,8 +106,9 @@ module SeasonStatsHelpers
   end
 
   def accuracy_percentage(season)
+    all_games_in_season(season)
     accuracy_percentage = {}
-    (accuracy_data(season)).each do |key, value|
+    accuracy_data.each do |key, value|
       accuracy_percentage[key] = (value[0].to_f / value[1]).round(4)
     end
     accuracy_sorted = accuracy_percentage.sort_by do |key, value|
@@ -120,9 +118,10 @@ module SeasonStatsHelpers
   end
 
   def hits_data(season)
+    all_games_in_season(season)
     hits_by_team = Hash.new {|hash, key| hash[key] = [0]}
     game_teams.each do |game|
-      if all_games_in_season(season).include?(game.game_id)
+      if @full_season_games.include?(game.game_id)
         hits_by_team[game.team_id][0] += game.hits
       end
     end
@@ -133,9 +132,10 @@ module SeasonStatsHelpers
   end
 
   def power_play_goal_data(season)
+    all_games_in_season(season)
     power_play_goals_data = Hash.new {|hash, key| hash[key] = [0,0]}
     game_teams.each do |game|
-      if all_games_in_season(season).include?(game.game_id)
+      if @full_season_games.include?(game.game_id)
         power_play_goals_data[season][1] += game.power_play_goals
         power_play_goals_data[season][0] += game.goals
       end
